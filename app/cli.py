c31362256dd95def80b80185e0c41bdcdaf44814
Mai <garcia.ton94@gmail.com>
2023-06-27 12:24:52 -0400

    f

--- app/cli.py
+++ app/cli.py
@@ -1,6 +1,6 @@
 from app import app
-from models import db
+from models import db, Company, Contact, JobApplication
 
 if __name__ == "__main__":
     with app.app_context():
-        pass
+        print("Is there a job status you want to update? (Y/n) ")
