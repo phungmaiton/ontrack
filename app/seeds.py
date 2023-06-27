b466a5a06cf869aef86e9fcc4a7feef1e2623abc
Mai <garcia.ton94@gmail.com>
2023-06-27 09:16:44 -0400

    added models and seeds

--- app/seeds.py
+++ app/seeds.py
@@ -1,6 +1,74 @@
 from app import app
-from models import db
+from models import db, Company, Contact, JobApplication
+from faker import Faker
+
+fake = Faker()
 
 if __name__ == "__main__":
     with app.app_context():
-        pass
+        Company.query.delete()
+        JobApplication.query.delete()
+        Contact.query.delete()
+
+        companies = [
+            Company(
+                name="Amazon",
+                website="https://amazon.com",
+                industry="Tech",
+                location="Arlington, VA",
+            ),
+            Company(
+                name="Netflix",
+                website="https://jobs.netflix.com",
+                industry="Entertainment",
+                location="Remote",
+            ),
+            Company(
+                name="Google",
+                website="https://careers.google.com",
+                industry="Tech",
+                location="Washington, DC",
+            ),
+        ]
+
+        db.session.add_all(companies)
+
+        contacts = []
+
+        for _ in range(3):
+            fake_contact = Contact(
+                name=fake.name(), email=fake.email(), phone_number=fake.phone_number()
+            )
+            contacts.append(fake_contact)
+
+        db.session.add_all(contacts)
+
+        job_applications = [
+            JobApplication(
+                job_title="Software Development Engineer",
+                application_date="6/27/2023",
+                status="Applied",
+                notes="N/A",
+                company_id=1,
+                contact_id=1,
+            ),
+            JobApplication(
+                job_title="Software Engineer",
+                application_date="5/27/2023",
+                status="Interview Scheduled",
+                notes="N/A",
+                company_id=2,
+                contact_id=2,
+            ),
+            JobApplication(
+                job_title="Systems Development Engineer, Google Cloud",
+                application_date="6/27/2023",
+                status="Applied",
+                notes="N/A",
+                company_id=3,
+                contact_id=3,
+            ),
+        ]
+
+        db.session.add_all(job_applications)
+        db.session.commit()
