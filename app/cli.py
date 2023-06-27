781f2d66bd816224835142e279171c9489a2f1b4
Mai <garcia.ton94@gmail.com>
2023-06-27 13:15:59 -0400

    added view_apps

--- app/cli.py
+++ app/cli.py
@@ -1,10 +1,5 @@
 from app import app
 from models import db, Company, Contact, JobApplication
-
-if __name__ == "__main__":
-    with app.app_context():
-        print("Is there a job status you want to update? (Y/n) ")
-
 from helpers import view_apps, add_app, view_apps_by_status
 
 if __name__ == "__main__":
@@ -13,16 +8,18 @@ if __name__ == "__main__":
         print("Hello, welcome to OnTrack - your personal job application manager!")
         print("*****")
 
+        applications = db.session.query(JobApplication)
+
         choice = input(
             f'Type "v" to view all of your current job applications, type "a" to add a new job application, or type "s" to view your applications based on application status: '
         )
         if choice.lower() == "v":
-            view_apps()
-        elif choice.lower() == "a":
-            add_app()
-        elif choice.lower() == "s":
-            view_apps_by_status()
-        else:
-            print(
-                'Please try again! Type "v" to view all of your current job applications, type "a" to add a new job application, or type "s" to view your applications based on application status: '
-            )
+            view_apps(applications)
+        # elif choice.lower() == "a":
+        #     add_app()
+        # elif choice.lower() == "s":
+        #     view_apps_by_status()
+        # else:
+        #     print(
+        #         'Please try again! Type "v" to view all of your current job applications, type "a" to add a new job application, or type "s" to view your applications based on application status: '
+        #     )
